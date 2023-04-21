1792dd2e82dca98c76b92bbe6431dd219f4536b2
asgdin <a1s2d3f3g4h5j6k7@gmail.com>
2023-04-21 17:42:30 +0300

    game loop

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import pygame
 
+pygame.init()
+screen = pygame.display.set_mode([500, 500])
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        pygame.display.flip()
+pygame.quit()
