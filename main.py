31ee1fc2769b5a45dfbfadc137983dfc8ff7a3dd
asgdin <a1s2d3f3g4h5j6k7@gmail.com>
2023-04-22 14:41:32 +0300

    click and avaivability check for sq 1

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode([1200, 1200])
-
-square1 = pygame.Rect(0,0, 400, 400)
-square2 = pygame.Rect(400, 0, 400, 400)
-square3 = pygame.Rect(800,0, 400, 400)
-square4 = pygame.Rect(0, 400, 400, 400)
-square5 = pygame.Rect(400,400, 400, 400)
-square6 = pygame.Rect(800, 400, 400, 400)
-square7 = pygame.Rect(0,800, 400, 400)
-square8 = pygame.Rect(400, 800, 400, 400)
-square9 = pygame.Rect(800,800, 400, 400)
+#rects
+square1ch = True
+square2ch = True
+square3ch = True
+square4ch = True
+square5ch = True
+square6ch = True
+square7ch = True
+square8ch = True
+square9ch = True
 
 x_or_o = 0
+def click_square():
+    mouse_pos = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()[0]
+    #rects
+
+    square1 = pygame.Rect(0, 0, 400, 400)
+    square2 = pygame.Rect(400, 0, 400, 400)
+    square3 = pygame.Rect(800, 0, 400, 400)
+    square4 = pygame.Rect(0, 400, 400, 400)
+    square5 = pygame.Rect(400, 400, 400, 400)
+    square6 = pygame.Rect(800, 400, 400, 400)
+    square7 = pygame.Rect(0, 800, 400, 400)
+    square8 = pygame.Rect(400, 800, 400, 400)
+    square9 = pygame.Rect(800, 800, 400, 400)
+    global square1ch
+
+    if click:
+        if square1.collidepoint(mouse_pos) and square1ch:
+            X_or_o(0, 0)
+            square1ch = False
+
 def X_or_o(x, y):
     global x_or_o
     if x_or_o % 2 == 0:
@@ -29,13 +50,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
         # lines
+        click_square()
         pygame.draw.rect(screen, 'white', (395,0, 10, 1200))
         pygame.draw.rect(screen, 'white', (795, 0, 10, 1200))
         pygame.draw.rect(screen, 'white', (0, 395, 1200, 10))
         pygame.draw.rect(screen, 'white', (0, 795, 1200, 10))
 
 
-        pygame.draw.rect(screen, 'white', square9)
+
 
     pygame.display.flip()
 pygame.quit()
